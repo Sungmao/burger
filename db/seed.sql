e660e137c4da258777c23ea7da1c4cdb2fb5a49c
Wu Sung-Mao <Allen@Wus-MacBook-Pro.local>
2016-11-11 21:53:40 -0600

    update function n handlebars

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken burger', false);
+
